04d5cff844e1a2f3c19ba5ad24039bbef9f23fcb
Carlos Amorim <carlos.amorim@sps.ce.gov.br>
2023-12-18 15:06:22 -0300

    Atualizações das configurações do Astro Neovim

--- plugins/user.lua
+++ plugins/user.lua
@@ -26,13 +26,6 @@ return {
   },
   {
     "mkropat/vim-ezguifont",
-    opts = {
-      font = "FiraCode Nerd Font Mono",
-      min = 10,
-      default = 14,
-      max = 24,
-      step = 1,
-    },
     keys = {
       { "<C-+>", "<cmd>IncreaseFont<cr>", desc = "Increase Font Size" },
       { "<C-->", "<cmd>DecreaseFont<cr>", desc = "Descrease Font Size" },
