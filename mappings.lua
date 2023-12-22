00cb3e13a634814c345d899b015e46af8244f54f
Carlos Amorim <carlos.amorim@sps.ce.gov.br>
2023-12-22 16:15:59 -0300

    New shortcut buffers

--- mappings.lua
+++ mappings.lua
@@ -30,6 +30,7 @@ return {
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
+    ["<F3>"] = { ":Telescope buffers<cr>" },
     ["<F4>"] = { ":Neotree source=last action=focus toggle=true<cr>" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
